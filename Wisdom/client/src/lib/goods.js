c09ab4ca23a61296a22af8f6c6514c956bab886c
miaoweishi <932514210@qq.com>
2020-01-15 21:36:37 +0800

    1.5.4

--- Wisdom/client/src/lib/goods.js
+++ Wisdom/client/src/lib/goods.js
@@ -24,7 +24,7 @@ $(() => {
 
     /*截取url的参数id，查看数据库渲染数据*/
     (function () {
-        var gid = decodeURI(location.search.slice(1));
+        let gid = decodeURI(location.search.slice(1));
         // console.log(gid);
         $.ajax({
             type: "get",
@@ -46,7 +46,7 @@ $(() => {
                 $(".topPriceBox").append(tpl1);
                 $(".item-row-w").eq(0).append(tpl2);
                 $(".oriPrice").text($("#oriPriceTop").text());
-
+                
                 /*导航栏的数据渲染*/
                 let html1 = arr.map(ele => `<a href="">${ele.newCname}</a>&gt;
                 <a href="" class="navTitle">${ele.title}</a>`).join("");
@@ -55,20 +55,74 @@ $(() => {
                 $("#comTitle").text($(".navTitle").text())
 
                 /*左边大图图片的渲染*/
-                let midtu = $("#bigpic .zoomPad img");
-                let bigtu = $("#bigpic .zoomWindow img"); //注意！！！
-                midtu.src = bigtu.src;
                 let html2 = arr.map(ele => {
-                    return ` <img src="${ele.url}" style="height: 380px; width: 380px; margin-left: 0px; margin-top: 0px; visibility: visible; opacity: 1;"><a href="" class="zoomPup" style="top: 166px; left: 48px; width: 213px; height: 213px; position: absolute; border-width: 1px; display: none;"></a>`
+                    return ` <img src="${ele.url}" style="height: 380px; width: 380px; margin-left: 0px; margin-top: 0px; visibility: visible; opacity: 1;">`
                 }).join("");
                 $(".zoomPad").append(html2);
+                let midtu = $("#bigpic .zoomPad img");
+                let bigtu = $("#bigpic .zoomWindow img"); //注意！！！
+                 bigtu.attr("src",midtu.attr("src")); //使中图和放大的图片一样
             }
         });
     })();
 
+    /*放大镜效果*/
+    //给左边的图片添加鼠标移入事件
+    $(".zoomPad").mouseenter(function () { 
+        $(".mask").css("display", "block");
+        $(".zoomWindow").css("display", "block");
+    });
+    
+    //给左边的图片添加鼠标移出事件
+    $(".zoomPad").mouseleave(function () { 
+        $(".mask").css("display", "none");
+        $(".zoomWindow").css("display", "none");
+    });
+
+    //给小图片盒子添加移动事件
+    $(".zoomPad").mousemove(function(e){
+         /* 得到遮罩物自身宽和高 */
+         let centerX = $(".mask").width();
+         let centerY = $(".mask").height();
+        
+         /* 得到距离浏览器的距离 */
+         let x = $(".zoomPad").offset().left;
+         let y = $(".zoomPad").offset().top;
+         
+         /* 得到遮罩物距左边盒子的距离 */
+         let left = e.pageX - x - centerX / 2;
+         let top = e.pageY - y - centerY / 2;
+
+         /* 遮罩物向x轴移动的最大距离 */
+         let minPicX = $(".zoomPad").width() - centerX;
+         /* 遮罩物向Y轴移动的最大距离 */
+         let minPicY = $(".zoomPad").height() - centerY;
+
+         /* 设置临界值 */
+         if (left <= 0) {
+             left = 0
+         } else if (left > minPicX) { //向左运动的最大距离
+             left = minPicX;
+         }
+         if (top <= 0) {
+             top = 0;
+         } else if (top >= minPicY) { //向下运动的最大距离
+             top = minPicY;
+         }
+
+         $(".mask").css("left", left).css("top", top);
+        
+        /* 得到大图片移动的最大距离 */
+        let maxPicX = $(".zoomWindow img").width() - $(".zoomWindow").width();
+        let maxPicY = $(".zoomWindow img").height() - $(".zoomWindow").height() ;
+        /* 得到显示区域的的移动的图片 */
+        let leftImg = -left / minPicX * maxPicX;
+        let topImg = -top / minPicY * maxPicY;
+        $(".zoomWindow img").css("left", leftImg).css("top", topImg);
+    })
+
     /*轮播小图的切换*/
     // (function swipe() {
-
     //     /*点击左右按钮切换图片*/
     //     let index = 0;
     //     $("#baseNex").click(function(){
@@ -80,30 +134,56 @@ $(() => {
     // })()
 
     /*商品数量加减*/
-    $(".count-up").click(function () {
+    let countbox = $(".count-input"); //单个数量
+    // let num = $(".item-left-t").siblings().text(); //总数量
+    $(".count-down").attr("disabled", true);
+    $(".count-up").attr("disabled", false);
+    // let sum = $("#totalPrice").text();
+    let price = $("#oriPriceTop");
+    // console.log(price);
+    // let price = String($(".oriPrice").text());
+    // console.log(price);
+    // let gprice = price.match(/\d+/);
+    // console.log(gprice);
 
-        this.prev().val()
+    //数量增加
+    $(".count-up").click(function () {
+        countbox.val(Math.abs(parseInt(countbox.val())) + 1)
+        if (parseInt(countbox.val()) != 1) {
+            $(".count-down").attr('disabled', false);
+            $(".count-down").removeClass("count-down-limit");
+
+            //控制总价的变化
+            // console.log( ($("#oriPriceTop").text()));
+            // sum = countbox.val() * $("#oriPriceTop").text();
+        } 
+       
+    })
+    //数量减少
+    $(".count-down").click(function () {
+        countbox.val(Math.abs(parseInt(countbox.val())) - 1);
+        if (parseInt(countbox.val()) == 1) {
+            $(".count-down").attr('disabled', true);
+            $(".count-down").addClass("count-down-limit");
+
+            sum = countbox.val() * $(".oriPrice").text()
+        };
     })
 
-    $(document).ready(function () {
-        //获得文本框对象
-        var t = $("#text_box");
-        //初始化数量为1,并失效减
-        $('#min').attr('disabled', true);
-        //数量增加操作
-        $("#add").click(function () {
-            // 给获取的val加上绝对值，避免出现负数
-            t.val(Math.abs(parseInt(t.val())) + 1);
-            if (parseInt(t.val()) != 1) {
-                $('#min').attr('disabled', false);
-            };
-        })
-        //数量减少操作
-        $("#min").click(function () {
-            t.val(Math.abs(parseInt(t.val())) - 1);
-            if (parseInt(t.val()) == 1) {
-                $('#min').attr('disabled', true);
-            };
-        })
-    });
+
+    /*点击加入购物车跳转到购物车页*/
+    $(".setMyCart").click(function(){
+        let gid = decodeURI(location.search.slice(1));
+        // $.ajax({
+        //     type: "post",
+        //     url: "../../../server/insertData05.php",
+        //     data: {
+        //         gid:gid
+        //     },
+        //     success: function (response) {
+                
+        //     }
+        // });
+        window.location.href="./cart.html?gid=" + gid;
+    })
 });
---
+++ Wisdom/server/insertData05.php
@@ -0,0 +1,43 @@
+<?php
+    /*
+        php方式操纵数据库：
+            1.连接数据库
+            2.书写查询语句->执行语句->得到结果->返回给前端
+            3.关闭连接
+    */
+    include 'conn.php';//引入外部文件
+    // var_dump($conn);
+   $gid = isset($_REQUEST['gid'])?$_REQUEST['gid'] : '';
+   $uid = isset($_REQUEST['uid'])?$_REQUEST['uid'] : '';
+   $title = isset($_REQUEST['title'])?$_REQUEST['title'] : '';
+   $url = isset($_REQUEST['url'])?$_REQUEST['url'] : '';
+   $price = isset($_REQUEST['price'])?$_REQUEST['price'] : '';
+   $num = isset($_REQUEST['num'])?$_REQUEST['num'] : '';
+   $newCname = isset($_REQUEST['newCname'])?$_REQUEST['newCname'] : '';
+//    var_dump($gid);
+    //2.书写查询语句->执行语句->得到结果->返回给前端
+    // $sql = "insert * FROM list WHERE id = $gid" ;
+    $sql = "INSERT INTO cart (id,uid,title,url,price,num,newCname) VALUES ($gid,$uid,'$title','$url','$price','$num','$newCname')";
+    //记得现在navicat检测没有问题再拿过来用
+
+    //执行语句
+    $res = $conn->query($sql);//得到的是结果集，如果想要查询到的数据部分还有继续提取
+
+    // var_dump($res);
+    $arr = mysqli_fetch_all($res,MYSQLI_ASSOC);//得到数组  [{},{},{}]
+    
+    // $data = str_split($arr)
+    // var_dump($data);
+    //查询前设置编码，防止输出乱码
+    $conn->set_charset('utf8mb4');
+    
+    //返回给前端
+    echo json_encode($arr,JSON_UNESCAPED_UNICODE);//把数组转成字符串，传给前端
+
+    //3.关闭连接
+    // $res>close();//关闭结果集
+    $conn->close();//关闭数据库
+
+?>
+
+
